2a0995d1d237fe7fd99d0ebdd2622ff46ab8f15a
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-07-19 15:52:18 -0300

    Finalizado

--- src/views/MarcaView.vue
+++ src/views/MarcaView.vue
@@ -3,7 +3,7 @@ import { ref, reactive, onMounted } from "vue";
 import MarcasApi from "@/api/marcas";
 const marcasApi = new MarcasApi();
 
-const defaultMarca = { id: null, nome: "" };
+const defaultMarca = { id: null, nome: "", nacionalidade: "" };
 const marcas = ref([]);
 const marca = reactive({ ...defaultMarca });
 
@@ -41,6 +41,7 @@ async function excluir(id) {
   <hr />
   <div class="form">
     <input type="text" v-model="marca.nome" placeholder="Nome" />
+    <input type="text" v-model="marca.nacionalidade" placeholder="Nacionalidade" />
     <button @click="salvar">Salvar</button>
     <button @click="limpar">Limpar</button>
   </div>
@@ -48,7 +49,7 @@ async function excluir(id) {
   <ul>
     <li v-for="marca in marcas" :key="marca.id">
       <span @click="editar(marca)">
-        ({{ marca.id }}) - {{ marca.nome }} -
+        ({{ marca.id }}) - {{ marca.nome }} - {{ marca.nacionalidade }}
       </span>
       <button @click="excluir(marca.id)">X</button>
     </li>
